1acdb5be58b9d6cb50882f820595e2ec68341171
Guilherme Silveira <108364424+SilveiraGuilherme@users.noreply.github.com>
2025-02-04 14:47:33 +0000

    Updated Algorithms

--- Algorithms And Advanced Programming/Lab2/Lab2.java
+++ Algorithms And Advanced Programming/Lab2/Lab2.java
@@ -10,17 +10,44 @@ public class Lab2 {
     }
 
     public static long sumConstTime(int n) {
-        long num = ((n - (n % 2)) * ((n - (n % 2)) + 2)) / 4;
+        // gauss's formula sum = n(n + 1)/2
+        // sum even numbers -> (n / 2) * ((n / 2) + 1); (n - n%2) to make sure n is even
+        long k = (n - (n % 2)) / 2;
+        long num = (k * (k + 1));
         return num;
+    }
+
+    public static String concatDigits(int n) {
+        StringBuilder sb = new StringBuilder("");
+        // int nComb = (n + 1) * (n + 1);
+
+        for (int i = n; i >= 0; i--) {
+            for (int j = n; j >= 0; j--) {
+                sb.append(i).append(j).append(" ");
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
+
+    // to be implemented
+    public static String plateNumber() {
+        return "";
+    }
+
+    // to be implemented
+    public String eliminateHalf(int n) {
+        return "";
     }
 
     public static void main(String[] args) {
 
         long startTime = System.nanoTime();
-        long sumPosN = sumConstTime(328472853);
+        // long sumPosN = sumConstTime(829087498);
+        String digSeq = concatDigits(9);
         long elapsedTime = System.nanoTime() - startTime;
 
-        System.out.println(sumPosN);
+        System.out.println(digSeq);
         System.out.println("The elapsed time to perform this operation was " + elapsedTime + " nanoseconds.");
     }
 }
